366272af17fbeb0f4f16d7f725a8a14a420faa69
Paru - Paulo Victor Pinheiro <paruvitu@gmail.com>
2022-05-15 22:14:42 -0300

    forEach

--- server.js
+++ server.js
@@ -4,7 +4,35 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-  res.render("pages/index")
+  const items = [
+    {
+      title: "D",
+      message: "Desenvolver Aplicações/servicos mais facil"
+    },
+    {
+      title: "E",
+      message: "EJS usa javaScritp para renderizar HTML"
+    },
+    {
+      title: "M",
+      message: "Muito tranquilo de usar em seu site"
+    },
+    {
+      title: "A",
+      message: "Atualizações mais simples por componetização"
+    },
+    {
+      title: "i",
+      message: "Incrivelmente simples de implemantar novas páginas"
+    },
+    {
+      title: "S",
+      message: "Sintaxe simples"
+    }
+  ];
+  res.render("pages/index", {
+    qualitys:items,
+  })
 })
 
 app.get("/sobre", function(req, res){
